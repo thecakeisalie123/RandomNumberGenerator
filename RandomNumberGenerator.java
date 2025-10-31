3ff7edf7c3d3883eac0effcc2d7e529732db44d6
thecakeisalie123 <80518978+thecakeisalie123@users.noreply.github.com>
2025-10-31 10:31:53 +0000

    100 again.

--- RandomNumberGenerator.java
+++ RandomNumberGenerator.java
@@ -6,8 +6,8 @@ public class RandomNumberGenerator {
         Random random = new Random();
 
         // Generate and print 100 random numbers
-        System.out.println("Generating 50 random numbers:");
-        for (int i = 1; i <= 50; i++) {
+        System.out.println("Generating 100 random numbers:");
+        for (int i = 1; i <= 100; i++) {
             // Generate a random number (can customize range if needed)
             int randomNumber = random.nextInt();
             System.out.println("Number " + i + ": " + randomNumber);
